54add3c1ca5770e0af65241d5245b8c7f25ed2c8
ShrivatsaMishra <shrivatsamishra@gmail.com>
2021-04-16 22:04:13 +0530

    ads

--- website/auth.py
+++ website/auth.py
@@ -18,3 +18,19 @@ def signup():
 @auth.route('/customer_signup')
 def custumer_signup():
     return render_template("customer_signup.html")
+
+@auth.route('/chef_signup')
+def chef_signup():
+    return render_template("chef_signup.html")
+
+@auth.route('/delivery_signup')
+def delivery_signup():
+    return render_template("delivery_signup.html")
+
+@auth.route('/manager_signup')
+def manager_signup():
+    return render_template("manager_signup.html")
+
+@auth.route('/publicity_signup')
+def publicity_signup():
+    return render_template("publicity_signup.html")
